6665d598ab3e1694a8046f951886162e82a255f0
耳眼心口 <108947075+3261302675@users.noreply.github.com>
2022-11-10 23:18:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3|世界|上最|漂亮|的|雅璐|祝你|生日|快乐|#circle|');
+      S.UI.simulate('|#countdown 3｜|世界|上最|漂亮|的｜女生|雅璐|祝你|生日|快乐|#circle|');
     }
 
     S.Drawing.loop(function () {
